8ad9437f159b2af87aa7d4a5944225b393b0f304
KevinCondor <kevin.condor@epn.edu.ec>
2025-02-15 15:54:12 -0500

    feat: Actualizar la forma de comunicación con los otros microservicios

--- apps/auth-ms/src/app/auth/auth.service.ts
+++ apps/auth-ms/src/app/auth/auth.service.ts
@@ -5,10 +5,9 @@ import { JwtService } from '@nestjs/jwt';
 import { firstValueFrom } from 'rxjs';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
-import { envs } from '../../config';
+import { envs, URL_BASE } from '../../config';
 import { CreatePetOwnerDto } from './dto/create-pet-owner.dto';
 import { CreateEntrepreneurDTO } from './dto/create-entrepreneur.dto';
-import { HttpException, HttpStatus } from '@nestjs/common'; 
 
 @Injectable()
 export class AuthService {
@@ -49,7 +48,7 @@ export class AuthService {
         const { email, password } = loginUserDto;
         console.log(email);
         const user = await firstValueFrom(
-            this.httpService.post('http://localhost:3001/api/users/admin-by-email', {
+            this.httpService.post(`${URL_BASE}users/admin-by-email`, {
                 email,
             })
         )
@@ -78,7 +77,7 @@ export class AuthService {
     async loginPetOwner(loginUserDto: LoginDto) {
         const { email, password } = loginUserDto;
         const user = await firstValueFrom(
-            this.httpService.post('http://localhost:3001/api/users/pet-owner-by-email', {
+            this.httpService.post(`${URL_BASE}users/pet-owner-by-email`, {
                 email,
             })
         )
@@ -105,82 +104,82 @@ export class AuthService {
         return petOwnerData;
     }
 
-  async registerPetOwner(createPetOwnerDto: CreatePetOwnerDto) {
-    const { ...petOwner } = createPetOwnerDto;
-    const user = await firstValueFrom(
-      this.httpService.post('http://localhost:3001/api/users/create-pet-owner', petOwner)
-    )
-    console.log(user.data);
-    return user.data;
-  }
-
-  async loginEntrepreneur(loginUserDto: LoginDto) {
-    const { email, password } = loginUserDto;
-    console.log(email);
-
-    try {
+    async registerPetOwner(createPetOwnerDto: CreatePetOwnerDto) {
+        const { ...petOwner } = createPetOwnerDto;
         const user = await firstValueFrom(
-            this.httpService.post('http://localhost:3001/api/users/find-entrepreneur-by-email', { email })
+            this.httpService.post(`${URL_BASE}users/create-pet-owner`, petOwner)
+        )
+        console.log(user.data);
+        return user.data;
+    }
+
+    async loginEntrepreneur(loginUserDto: LoginDto) {
+        const { email, password } = loginUserDto;
+        console.log(email);
+
+        try {
+            const user = await firstValueFrom(
+                this.httpService.post(`${URL_BASE}users/find-entrepreneur-by-email`, { email })
+            );
+
+            if (!user.data) {
+                throw new RpcException({
+                    status: 404,
+                    message: 'Usuario no encontrado'
+                });
+            }
+
+            if (user.data.estado !== 'APPROVED') {
+                throw new RpcException({
+                    status: 403,
+                    message: `No es posible iniciar sesión. Estado de la cuenta: Pendiente. Espera aprobación del administrador.`
+                });
+            }
+
+            const isPasswordValid = bcrypt.compareSync(password, user.data.password);
+            if (!isPasswordValid) {
+                throw new RpcException({
+                    status: 401,
+                    message: 'Contraseña incorrecta'
+                });
+            }
+
+            const { password: _, ...result } = user.data;
+            const token = await this.signJWT(result.email);
+
+            const entrepreneurData = {
+                id: result.id,
+                email: result.email,
+                idEntrepreneur: result.idEntrepreneur,
+                token: token
+            };
+
+            return entrepreneurData;
+        } catch (error) {
+            console.error('Error en loginEntrepreneur:', error);
+
+            if (error instanceof RpcException) {
+                throw error;
+            }
+
+            throw new RpcException({
+                status: 500,
+                message: 'Error interno en el servidor. Por favor, intenta de nuevo más tarde.'
+            });
+        }
+    }
+
+
+
+    async registerEntrepreneur(createEntrepreneurDto: CreateEntrepreneurDTO) {
+        const { ...entrepreneur } = createEntrepreneurDto;
+
+        const user = await firstValueFrom(
+            this.httpService.post(`${URL_BASE}users/create-entrepreneur`, entrepreneur)
         );
 
-        if (!user.data) {
-            throw new RpcException({
-                status: 404,
-                message: 'Usuario no encontrado'
-            });
-        }
-
-        if (user.data.estado !== 'APPROVED') {
-            throw new RpcException({
-                status: 403,
-                message: `No es posible iniciar sesión. Estado de la cuenta: Pendiente. Espera aprobación del administrador.`
-            });
-        }
-
-        const isPasswordValid = bcrypt.compareSync(password, user.data.password);
-        if (!isPasswordValid) {
-            throw new RpcException({
-                status: 401,
-                message: 'Contraseña incorrecta'
-            });
-        }
-
-        const { password: _, ...result } = user.data;
-        const token = await this.signJWT(result.email);
-
-        const entrepreneurData = {
-            id: result.id,
-            email: result.email,
-            idEntrepreneur: result.idEntrepreneur,
-            token: token
-        };
-
-        return entrepreneurData;
-    } catch (error) {
-        console.error('Error en loginEntrepreneur:', error);
-
-        if (error instanceof RpcException) {
-            throw error; 
-        }
-
-        throw new RpcException({
-            status: 500,
-            message: 'Error interno en el servidor. Por favor, intenta de nuevo más tarde.'
-        });
+        console.log('Entrepreneur registered:', user.data);
+        return user.data;
     }
-}
-
-
-
-async registerEntrepreneur(createEntrepreneurDto: CreateEntrepreneurDTO) {
-  const { ...entrepreneur } = createEntrepreneurDto;
-
-  const user = await firstValueFrom(
-    this.httpService.post('http://localhost:3001/api/users/create-entrepreneur', entrepreneur)
-  );
-
-  console.log('Entrepreneur registered:', user.data);
-  return user.data;
-}
 
 }
---
+++ apps/auth-ms/src/config/service.ts
@@ -0,0 +1,4 @@
+// const URL_BASE = 'http://localhost:3001/api/';
+const URL_BASE = 'http://api-gateway:3001/api/';
+
+export {URL_BASE};
